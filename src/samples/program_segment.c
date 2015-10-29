4aa2d34018ad65dc003cc14ecd961a68af45f79a
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-10-29 02:02:45 -0200

    samples: program print the addres of program's segments
    The program_segments.c prints the address of the program's segments.
    For this, it uses the end, edata and etext symbols.

---
+++ src/samples/program_segment.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char etext, edata, end;
+
+void print_segments();
+
+int main(int argc, char** argv)
+{
+	printf("First address past:\n");
+	printf("    program text (etext)      %10p\n", &etext);
+	printf("    initialized data (edata)  %10p\n", &edata);
+	printf("    uninitialized data (end, BBS)  %10p\n", &end);
+	printf("    sbrk(0)  %10p\n", sbrk(0));
+	void* memall = malloc(131072);
+	printf("After memory allocation :\n");
+	printf("    sbrk(0)  %10p\n", sbrk(0));
+	void* memall2 = malloc(131072);
+	printf("After memory allocation :\n");
+	printf("    sbrk(0)  %10p\n", sbrk(0));
+	free(memall2);
+	printf("After freed memory allocation :\n");
+	printf("    sbrk(0)  %10p\n", sbrk(0));
+	free(memall);
+	printf("After freed memory allocation :\n");
+	printf("    sbrk(0)  %10p\n", sbrk(0));
+	exit(EXIT_SUCCESS);
+}
