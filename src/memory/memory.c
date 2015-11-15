4312622bf4bea469a300dd0f0a6fd0c1ad5956c0
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-11-15 16:53:40 -0200

    memory.c: improve free list fragmentation
    memory_alloc and memory_free has been updated to avoid fragmentation
    in the free list. Now the memory_free function merges memory blocks
    neighbors

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -8,90 +8,80 @@
 #include <stdio.h>
 #endif
 
-#define HEADER_SIZE sizeof(struct Header)
+#define HEADER_SIZE sizeof(Header)
+#define MIN_ALLOC HEADER_SIZE * 10
 
-struct Header {
+struct header {
 	size_t length; //memory block length in bytes
-	struct Header *previous; //previous memory block in the free list
-	struct Header *next; //next memory block in the free list
+	struct header *next; //next memory block in the free list
 };
+typedef struct header Header;
+
 #ifdef DEBUG
-void show_free_memory_blocks(void);
+static void show_debug_info(void);
 #endif
 
-static struct Header *free_list = NULL;
+static Header *free_list = NULL;
+static Header base;
 
 void* memory_alloc(size_t bytes)
 {
+	Header *block;
+	Header *previous = &base;
 	if(!free_list){
 		//there isn't any memory block available
-		free_list = (struct Header*) sbrk(bytes + HEADER_SIZE);
-		free_list->length = bytes;
-		free_list->previous = NULL;
-		free_list->next = NULL;
+		Header *block;
+		if(bytes <= MIN_ALLOC)
+			block = (Header*) sbrk(MIN_ALLOC);
+		else
+			block = (Header*) sbrk(bytes * MIN_ALLOC);
+		block->length = bytes;
+		block->next = &base;
+		base.length = 0;
+		base.next = block;
+		free_list = &base;
 	}
-	struct Header *header = free_list;
 	//look for a memory block with enough length
-	while(header && (header->length < bytes || (header->length > bytes && (header->length - bytes < HEADER_SIZE + 1))))
-		header = header->next;
-	int extra = 0;
-	if(header && (extra = header->length - bytes) > 0){
-		//memory block is bigger than needed
-		struct Header *new_free_block = (struct Header*) (((char*)header) + HEADER_SIZE + bytes) ;
-		new_free_block->length = extra - HEADER_SIZE;
-		new_free_block->next = header->next;
-		header->next = new_free_block;
-		new_free_block->previous = header;
-	} else if(extra < 0 || !header){
-		//need more memory
-		header = (struct Header*) sbrk(bytes + HEADER_SIZE);
-		header->length = bytes;
-		header->previous = NULL;
-		header->next = NULL;
-	}
-	if(header->previous)
-		header->previous->next = header->next;
-	if(header->next)
-		header->next->previous = header->previous;
-	header->length = header->length - extra;
-	if(header == free_list) {
-		free_list = header->next;
-		if(free_list)
-			free_list->previous = NULL;
-
-	}
+	for(block = base.next; block != &base; block = block->next, previous = block){
+		if(bytes <= block->length){
+			if(bytes == block->length){ //the current memory block has exactly size! \o/
+				previous->next = block->next;
+				block->next = NULL;
 #ifdef DEBUG
-show_free_memory_blocks();
+				show_debug_info();
 #endif
-	return (void*) (((char*)header) + HEADER_SIZE);
+				return block + 1;
+			}
+			//the block is bigger. Let's split it
+			Header *remain_block = block + 1 + bytes;
+			remain_block->length = block->length - bytes;
+			remain_block->next = block->next;
+			previous->next = remain_block;
+#ifdef DEBUG
+			show_debug_info();
+#endif
+			return block + 1;
+		}
+	}
 }
 
 void memory_free(void* ptr)
 {
-	struct Header* memory_block = (struct Header*) (((char*)ptr) - HEADER_SIZE);
-	memory_block->previous = NULL;
-	memory_block->next = free_list;
-	if(memory_block->next)
-		memory_block->next->previous = memory_block;
-	free_list = memory_block;
-	ptr = NULL;
-#ifdef DEBUG
-show_free_memory_blocks();
-#endif
-
-}
-
-#ifdef DEBUG
-void show_free_memory_blocks()
-{
-	struct Header *header = free_list;
-	int total = 0;
-	//look for a memory block with enough memory
-	while(header){
-		printf("-> %zu bytes ", header->length);
-		header = header->next;
-		total += 1;
+	Header *free_block = ((Header*)ptr) - 1;
+	Header *block;
+	Header *previous = &base;
+	for(block = base.next; block != &base; block = block->next){
+		//let's find an adjacent memory block
+		if((block + 1 + block->length) == free_block){
+			block->length += free_block->length;
+		} else if((block - block->length) == free_block){
+			free_block->length += block->length;
+			free_block->next = block->next;
+			previous->next = free_block;
+		}
 	}
-	printf("\n");
 }
+
+#ifdef DEBUG
+static void show_debug_info(){}
 #endif
