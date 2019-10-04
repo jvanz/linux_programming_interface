5111c20cc945b6dfce57285fe1d93de401a19662
José Guilherme Vanz <jvanz@jvanz.com>
2019-10-04 17:29:08 -0300

    udpsckmodule: creating udp socket
    Adds the code to create an UDP socket and waits for read the data from it.
    This code has not been test yet.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- kernel-modules/udpsckmodule.c
+++ kernel-modules/udpsckmodule.c
@@ -1,3 +1,5 @@
+// https://kernelnewbies.org/Simple_UDP_Server
+// https://github.com/esposem/Kernel_UDP
 #include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -7,19 +9,63 @@
 #include <linux/types.h>
 
 #define MODULE_NAME "testudpserver"
+#define DEFAULT_PORT 8088
+#define BUFFER_SIZE 128
 
 struct mythread_t 
 {
 	struct task_struct *thread;
+	struct socket *sock;
+	struct sockaddr_in addr;
 };
 
 struct mythread_t *kthread = NULL;
 
-static void server_start(void) 
+static void server(void) 
 {
-	while(1){}
-}
+	int err, size;
+	unsigned char buf[BUFFER_SIZE+1];
+	struct msghdr msg;
+	struct kvec vec;
 
+	// create a socket
+	if ((err = sock_create(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &kthread->sock)) < 0) {
+		printk(KERN_INFO "%s: Cannot create socket.\n", MODULE_NAME);
+		goto out;
+	}
+	memset(&kthread->addr, 0, sizeof(struct sockaddr));
+	kthread->addr.sin_family = AF_INET;
+	kthread->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	kthread->addr.sin_port = htons(DEFAULT_PORT);
+
+	if ((err = kthread->sock->ops->bind(kthread->sock, 
+		(struct sockaddr *)&kthread->addr, sizeof(struct sockaddr))) < 0) {
+		printk(KERN_INFO "%s: Cannot bind socket.\n", MODULE_NAME);
+		goto close_and_out;
+	}
+	printk(KERN_INFO "%s: listening on port %d.\n", MODULE_NAME, DEFAULT_PORT);
+
+	for(;;) {
+		memset(&buf, 0, BUFFER_SIZE+1);
+		vec.iov_len = BUFFER_SIZE;
+		vec.iov_base = buf;
+		size = kernel_recvmsg(kthread->sock, &msg, &vec, 1, BUFFER_SIZE, MSG_WAITALL);
+		if (size < 0) {
+			printk(KERN_INFO "%s: error getting datagram, sock_recvmsg error = %d.\n", MODULE_NAME, size);
+		} else {
+			printk(KERN_INFO "%s: received %d bytes.\n", MODULE_NAME, size);
+			printk(KERN_INFO "%s: data %s.\n", MODULE_NAME, buf);
+			
+		}
+	}
+close_and_out:
+        sock_release(kthread->sock);
+        kthread->sock = NULL;
+
+out:
+        kthread->thread = NULL;
+
+}
 
 int __init udpserver_init(void)
 {
@@ -27,7 +73,7 @@ int __init udpserver_init(void)
 	kthread = kmalloc(sizeof(struct mythread_t), GFP_KERNEL);
 	memset(kthread, 0, sizeof(struct mythread_t));
 
-	kthread->thread = kthread_run((void*)server_start, NULL, MODULE_NAME);
+	kthread->thread = kthread_run((void*)server, NULL, MODULE_NAME);
 	if (IS_ERR(kthread->thread)) {
 		printk(KERN_INFO "%s: cannot start kthread\n", MODULE_NAME);
 		kfree(kthread);
